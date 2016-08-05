dd66a817bda227165da6a78e8585773123a54697
Forrest <forrest.corry@gmail.com>
2016-08-05 15:36:58 -0400

    adventure mode

--- PigLatin.java
+++ PigLatin.java
@@ -1,32 +1,112 @@
 package com.ssa.day4.assignment;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PigLatin {
-
-	public static Scanner sc = new Scanner(System.in);
-
+	
 	public static void main(String[] args) {
+		char firstLetter;
+		char secondLetter;
 		
+		Scanner keyboard = new Scanner(System.in);
+		String enter;
 		
+		int length;
 		
-		System.out.println("Please type in a word and I will translate it to Pig Latin");
+		int wordsNumber;
+		System.out.println("Enter in the number of words you would like to add, followed by your words");
+		wordsNumber = Integer.parseInt(keyboard.nextLine());
 		
-		String userInput = sc.nextLine();
+		String inputs[] = new String[wordsNumber];
+		String inputs2[] = new String[wordsNumber];
 		
-		
-		char firstLetter = userInput.charAt(0);
-		if (firstLetter == 'a' || firstLetter == 'e' || firstLetter == 'i' || firstLetter == 'o' || firstLetter == 'u' || firstLetter == 'y'
-				|| firstLetter == 'A' || firstLetter == 'E' || firstLetter == 'I' || firstLetter == 'O' || firstLetter == 'U' || firstLetter == 'Y') {
-			userInput = userInput.concat("-yay");
-			System.out.println("The word you passed is  : " + userInput + " in Pig Latin");
+		for (int i = 0; i < wordsNumber; i++) {
+			
+			inputs[i] = keyboard.nextLine();
+			enter = inputs[i];
+			StringBuilder pigLatinChange = new StringBuilder(enter);
+			
+			firstLetter = pigLatinChange.charAt(0);
+			secondLetter = pigLatinChange.charAt(1);
+			
+			if (((consanantsAndVowels(firstLetter) == false)) && (consanantsAndVowels(secondLetter) == false)) {
+				
+				pigLatinChange = pigLatinChange.deleteCharAt(0);
+				length = pigLatinChange.length();
+				
+				pigLatinChange = pigLatinChange.deleteCharAt(0);
+				
+				pigLatinChange = pigLatinChange.insert(pigLatinChange.length(), firstLetter + "" + secondLetter + "ay" );
+				inputs2[i] = pigLatinChange.toString();
+			}
+			else if (consanantsAndVowels(firstLetter) == false) {
+				pigLatinChange = pigLatinChange.deleteCharAt(0);
+				length = pigLatinChange.length();
+				pigLatinChange = pigLatinChange.insert(pigLatinChange.length(), firstLetter + "ay");
+				inputs2[i] = pigLatinChange.toString();
+			}
+			else {
+				pigLatinChange = pigLatinChange.insert(pigLatinChange.length(), "" + "ay");
+				inputs2[i] = pigLatinChange.toString();
+			}
 		}
-		else{
-			StringBuilder modInput = new StringBuilder(userInput);
-			modInput.deleteCharAt(0);
-			modInput.append("-" + firstLetter + "ay");
-			System.out.println("The word you passed is :" + modInput + " in Pig Latin");
-		}
 		
+		for (int j = 0; j < wordsNumber; j++) {
+			System.out.println(inputs[j] + " ");
+		}
+	}
+	
+	public static boolean consanantsAndVowels(char numberOneLetter){
+		boolean vowels = false;
+		if (numberOneLetter == 'a') {
+			vowels = true;
+			return vowels;
+		}
+		else if (numberOneLetter == 'A') {
+			vowels = true;
+			return vowels;
+		}
+		else if (numberOneLetter == 'e') {
+			vowels = true;
+			return vowels;
+		}
+		else if (numberOneLetter == 'E') {
+			vowels = true;
+			return vowels;
+		}
+		else if (numberOneLetter == 'i') {
+			vowels = true;
+			return vowels;
+		}
+		else if (numberOneLetter == 'I') {
+			vowels = true;
+			return vowels;
+		}
+		else if (numberOneLetter == 'o') {
+			vowels = true;
+			return vowels;
+		}
+		else if (numberOneLetter == 'O') {
+			vowels = true;
+			return vowels;
+		}
+		else if (numberOneLetter == 'u') {
+			vowels = true;
+			return vowels;
+		}
+		else if (numberOneLetter == 'U') {
+			vowels = true;
+			return vowels;
+		}
+		else if (numberOneLetter == 'y') {
+			vowels = true;
+			return vowels;
+		}
+		else if (numberOneLetter == 'Y') {
+			vowels = true;
+			return vowels;
+		}
+		return vowels;
 	}
 }
